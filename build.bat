af485c4c43b66b635b3ac8bdc93dd1ef034ad830
Riley Kuhlman <rilstarssun@gmail.com>
2022-06-14 20:09:45 -0500

    Improved build file

--- build.bat
+++ build.bat
@@ -1,3 +1,4 @@
+@REM Using -F makes it a one file application
 pyinstaller --icon rocket.ico -F -w main.py
 
 set originalPath=dist
@@ -19,4 +20,5 @@ rmdir ..\Builds\CEASimplified /S /Q
 
 move %originalPath% ..\Builds\CEASimplified
 
+@REM Necessary if you are not doing a one file build (I think)
 @REM rmdir dist
